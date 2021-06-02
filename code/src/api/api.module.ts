152d246592ce46fc11a3623d92639a9b4579925e
Héctor Martín Armas <hector@acidtango.com>
2021-06-02 16:07:46 +0100

    feat: add health check

--- code/src/api/api.module.ts
+++ code/src/api/api.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 import DatabaseConnectionModule from '../shared/infrastructure/database/relational/database-connection.module'
+import HealthModule from './health/health.module'
 
 @Module({
-  imports: [DatabaseConnectionModule],
+  imports: [DatabaseConnectionModule, HealthModule],
   controllers: [],
 })
 export default class ApiModule {}
