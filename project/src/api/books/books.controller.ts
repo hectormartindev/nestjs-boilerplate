d8171ed59bd6f0a08e052f07349919d482730d9b
Borja Nebbal <borja@acidtango.com>
2021-06-15 11:22:20 +0200

    feat: implement get book by uuid endpoint

--- project/src/api/books/books.controller.ts
+++ project/src/api/books/books.controller.ts
@@ -1,14 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, Get, Param, Post } from '@nestjs/common'
 import AddBook from '../../books/application/add-book.usecase'
+import FindBookByUuid from '../../books/application/find-book-by-uuid.usecase'
 import Book from '../../books/domain/book.domain'
 import CreateBookDto from './dtos/create-book.dto'
 
 @Controller('books')
 export default class BooksController {
-  constructor(private readonly addBook: AddBook) {}
+  constructor(private readonly addBook: AddBook, private readonly findBookByUuid: FindBookByUuid) {}
 
-  @Post('/')
+  @Post()
   async createBook(@Body() body: CreateBookDto): Promise<Book> {
     return this.addBook.execute(body)
+  }
+
+  @Get('/:uuid')
+  async getBookByUuid(@Param() uuid: string): Promise<Book> {
+    return this.findBookByUuid.execute({ uuid })
   }
 }
